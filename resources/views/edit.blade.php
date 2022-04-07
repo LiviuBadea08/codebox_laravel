ed7429075e14649a84496b3f5c7d97e8f816770d
Hushino <abde95.inf3@gmail.com>
2022-04-07 15:50:51 +0200

    form

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -4,7 +4,7 @@
     <form method="post" action="{{route('update', $event->id)}}">
         @method('patch')
         @csrf
-        <div class="flex space-x-2 pl-16 pb-5 pt-5">
+        <div class="flex space-x-2 pl-16 ">
         <h1>Añadir un evento</h1>
         </div>
         <form class=" col-sm-4 p-4">
@@ -12,9 +12,9 @@
             <section class="flex space-x-2 justify-center pb-2">
                         <div class="marco1">
                             <label for="image_uploads">
-                                <img src="./media/plus.png" style="cursor:pointer">
-                            </label>
-                            <input type="file" id="image_uploads" name="image_uploads" accept=".jpg, .jpeg, .png" multiple style="display:none" value="{{$event->image}}">
+                                <img src="./img/plus.png" style="cursor:pointer">
+                            </label>    
+                            <input type="file" id="image_uploads" name="image_uploads" accept=".jpg, .jpeg, .png" style="display:none" required value="{{$event->image}}">
                         </div>
             </section>
                 <h3 class="flex space-x-2 justify-center">Titulo</h3>
@@ -33,10 +33,10 @@
             <section class="flex space-x-2 justify-center">
                     <div>
                         <label for="start"></label>
-                        <input type="date" id="start" name="trip-start"  min="2022-04-06" max="2022-12-31">
+                        <input type="date" id="start" name="trip-start"  min="1001-01-01" max="2200-12-31" required value="{{$event->date}}">
                     </div>
                     <div>
-                        <input type="time" name="hora" id="time">
+                        <input type="time" name="hora" id="time" required value="{{$event->time}}">
                     </div>
             </section>
             <section id="container" class="flex space-x-2 justify-center py-6">
@@ -44,14 +44,12 @@
                     <label><input type="checkbox" id="destacados">Destacado</label>
                 </div>
                 <div class="contenido marco2">
-                    <input type="number" placeholder="Maximo participantes" max="99" />
+                    <input type="number" placeholder="Maximo participantes" max="99" required value="{{$event->capacity}}"/>
                 </div>
             </section>
             <div class="flex space-x-2 justify-center space-evenly">
-                <button type="submit" class="inline-block px-6 py-2.5 bg-green-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out" onclick="return confirm ('Seguro que desea modificar este evento {{$event->name }}?')">Confirmar</button>
+                <button type="submit" method="update" class="inline-block px-6 py-2.5 bg-green-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out" onclick="return confirm ('Seguro que desea modificar este evento {{$event->name }}?')">Confirmar</button>
                 <a class="btn inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out" href="{{route('home')}}">cancelar</a>
             </div>
     </form>
-
-
 @endsection
