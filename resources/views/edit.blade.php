b1c56d868ee898339f26b844e6cdb84a33962b64
Oriol Codina Vallori <oriol.codina10@gmail.com>
2022-04-07 15:36:12 +0200

    fixes at home and edit views

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,13 +1,14 @@
 @extends('layouts.app')
 @section('content')
 <!-- create a form for updating the products -->
-    <form method="post" action="{{route('update', $event->id)}}">
+    <form>
+        
+        <div class="flex space-x-2 pl-16 pb-5 pt-5">
+        <h1>Editar evento: {{$event->name}}</h1>
+        </div>
+        <form method="post" action="{{route('update', $event->id)}}" class=" col-sm-4 p-4">
         @method('patch')
         @csrf
-        <div class="flex space-x-2 pl-16 pb-5 pt-5">
-        <h1>Añadir un evento</h1>
-        </div>
-        <form class=" col-sm-4 p-4">
             <h3 class="flex space-x-2 justify-center">Imagen</h3>
             <section class="flex space-x-2 justify-center pb-2">
                         <div class="marco1">
@@ -49,7 +50,7 @@
             </section>
             <div class="flex space-x-2 justify-center space-evenly">
                 <button type="submit" class="inline-block px-6 py-2.5 bg-green-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out" onclick="return confirm ('Seguro que desea modificar este evento {{$event->name }}?')">Confirmar</button>
-                <a class="btn inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out" href="{{route('home')}}">cancelar</a>
+                <a class="btn inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out" href="{{route('home')}}">Cancelar</a>
             </div>
     </form>
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,14 +4,13 @@
 <div class="container">
     <div class="flex items-center flex-wrap justify-around">
     @foreach ($events as $event)
-        
+
         <!-- Card -->
         <div class="hover:bg-gray-800 delay-50 duration-100 bg-gray-900 p-4 rounded-lg max-w-sm group mb-8">
+
             <!-- Image Cover -->
-            <a href="#">
-                <img src="{{ $event -> image }}" style="width:100%; height:181px" class="w-full rounded shadow"/>
-            </a>
-            
+            <img src="{{ $event -> image }}" style="width:100%; height:181px" class="w-full rounded shadow"/>
+                
             <!-- Title -->
             <h3 class="text-gray-200 font-bold mt-3 text-center truncate_title"> {{ $event -> name }} </h3>
 
@@ -19,16 +18,16 @@
             <p class="text-gray-400 font-light mt-2 text-xs truncate_text mb-1"> {{ $event -> description }} </p>
 
             <div class="flex items-end justify-between">
-                <a target="_blank" rel="noreferrer noopener" href="#"
+                <a rel="noreferrer noopener" href="{{ route('show', ['id' => $event->id]) }}"
                     class="border-3 border-green-500 hover:bg-green-500 text-white rounded-full px-7 py-1">
-                    Ver mas
+                    Ver más
                 </a>
                 <div class="flex items-center flex-col">
                     <p class="text-gray-400 font-light">{{ $event -> date }}</p>
                     <p class="text-gray-400 font-light">Plazas: {{ $event -> capacity }}</p>
                 </div>
-                
-                <a target="_blank" rel="noreferrer noopener" href="{{ route('show', ['id' => $event->id]) }}"
+
+                <a rel="noreferrer noopener"
                     class="bg-blue-900 hover:bg-green-500 text-white rounded-full px-6 py-2">
                     Apuntarse
                 </a>
@@ -43,7 +42,7 @@
                     </button>
                 </form>
 
-                <a target="_blank" rel="noreferrer noopener" href="{{ route('edit', ['id' => $event->id]) }}"
+                <a rel="noreferrer noopener" href="{{ route('edit', ['id' => $event->id]) }}"
                     class=" text-white px-3 text-base">
                     <i class="fa-solid fa-pen-to-square"></i>
                 </a>
@@ -54,5 +53,7 @@
     @endforeach
     </div>
 </div>
+
 {{ $events -> links() }}
+
 @endsection
