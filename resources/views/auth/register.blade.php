06f7747eaff411260fad80c87596eb7eec1e2108
LiviuBadea08 <liviubadea08@gmail.com>
2022-04-11 14:06:31 +0200

    changed register button color

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container font-weight-bold">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -63,7 +63,7 @@
 
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="text-center w-full py-3 bg-green-400 hover:bg-green-400 rounded-md text-white">
                                     {{ __('Register') }}
                                 </button>
                             </div>
