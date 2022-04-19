5857c54ac7d3d26eb7e9d67dae15f0cd9e287cd7
Hushino <abde95.inf3@gmail.com>
2022-04-19 09:49:41 +0200

    edit_blade

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -5,33 +5,29 @@
 <!-- create a form to create an event with name, description, price, image, date, time, capacity, featured -->
 <form action="{{route('store')}}" method="post">
     @csrf
-        <div class="flex space-x-2 pl-16 ">
+        <div class="flex space-x-2 pl-16 font-bold">
         <h1>Añadir un evento</h1>
         </div>
         <form class=" col-sm-4 p-4">
-            <h3 class="flex space-x-2 justify-center">Imagen</h3>
+            <h3 class="flex space-x-2 justify-center font-bold pb-2">Imagen</h3>
             <section class="flex space-x-2 justify-center pb-2">
-                        <div class="marco1">
-                            <label for="image_uploads">
-                                <img src="./img/plus.png" style="cursor:pointer">
-                            </label>
-                            <input type="file" id="image_uploads" name="image_uploads" accept=".jpg, .jpeg, .png" multiple style="display:none">
+                        <div class="marco2">
+                            <input type="text" name="img">
                         </div>
-                
             </section>
-                <h3 class="flex space-x-2 justify-center">Titulo</h3>
+                <h3 class="flex space-x-2 justify-center font-bold pb-2">Titulo</h3>
             <section class="flex space-x-2 justify-center pb-2">
                         <div class="marco2">
                             <input type="text" name="titulo" id="titulo" aria-describedby="titulo" required>
                         </div>
             </section>
-                <h3 class="flex space-x-2 justify-center">Descripción</h3>
+                <h3 class="flex space-x-2 justify-center font-bold pb-2">Descripción</h3>
             <section class="flex space-x-2 justify-center pb-6">
                         <div class="marco2">
                             <textarea rows="4" cols="20" name="titulo" class="descripcion" id="titulo" aria-describedby="titulo" required></textarea>
                         </div>
             </section>
-                <h3 class="flex space-x-2 justify-center">Fecha y hora</h3>
+                <h3 class="flex space-x-2 justify-center font-bold pb-2">Fecha y hora</h3>
             <section class="flex space-x-2 justify-center">
                     <div>
                         <label for="start"></label>
@@ -42,8 +38,8 @@
                     </div>
             </section>
             <section id="container" class="flex space-x-2 justify-center py-6">
-                <div class="contenido pt-2">
-                    <label><input type="checkbox" id="destacados" >Destacado</label>
+                <div class="contenido pt-2 font-bold">
+                    <label><input type="checkbox" id="destacados">Destacado</label>
                 </div>
                 <div class="contenido marco2">
                     <input type="number" placeholder="Maximo participantes" max="99" />
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,50 +1,65 @@
 @extends('layouts.app')
 @section('content')
-
-    <form method="post" action="{{route('update', $event->id)}}">
-        @method('patch')
-        @csrf
-        <h1 class="flex pl-7">Editar un evento</h1>
-        <section>
-        <div class="flex justify-center space-x-2 pl-16 ">
-            <div class="marco2">
-                <img src="{{$event->image}}" alt="">
+    <main>
+        <form method="post" action="{{route('update', $event->id)}}">
+            @method('patch')
+            @csrf
+            <div class="flex space-x-2 pl-16 font-bold">
+                <h1>Editar un evento</h1>
             </div>
-        </div>
-        </section>
-        <h3 class="flex space-x-2 justify-center">Titulo</h3>
-        <section class="flex space-x-2 justify-center pb-2">
-            <div class="marco2">
-                <input type="text" class="form-control" id="name" name="name" value="{{$event->name}}">
-            </div>
-        </section>
-        <h3 class="flex space-x-2 justify-center">Descripción</h3>
-        <section class="flex space-x-2 justify-center pb-6">
+            <h3 class="flex space-x-2 justify-center pt-4 pb-2 font-bold">
+                Imagen
+            </h3>
+            <section class="flex space-x-2 justify-center pb-4">
                 <div class="marco2">
-                        <textarea rows="4" cols="25" name="titulo" class="descripcion" id="titulo" aria-describedby="titulo" required></textarea>
+                    <input type="text" name="img" value="{{$event->image}}">
                 </div>
-        </section>
-            <h3 class="flex space-x-2 justify-center">Fecha y hora</h3>
-        <section class="flex space-x-2 justify-center">
-        <div>
-            <label for="date"></label>
-            <input type="date" id="date" name="date" value="{{$event->date}}" min="0000-00-00" max="2022-12-31">
-        </div>
-        <div>
-            <input value="{{$event->time}}" type="time" name="time" id="time">
-        </div>
-        </section>
-        <section id="container" class="flex space-x-2 justify-center py-6">
-            <div class="contenido pt-2">
-                <label><input value="{{$event->fetured}}" type="checkbox" id="featured" >Destacado</label>
+            </section>
+            <h3 class="flex space-x-2 justify-center font-bold pb-2">
+                Titulo
+            </h3>
+            <section class="flex space-x-2 justify-center pb-4">
+                <div class="marco2">
+                    <input type="text" class="form-control" id="name" name="name" value="{{$event->name}}">
+                </div>
+            </section>
+            <h3 class="flex space-x-2 justify-center font-bold pb-2">
+                Descripción
+            </h3>
+            <section class="flex space-x-2 justify-center pb-6">
+                <div class="marco2">
+                    <textarea type="text" class="form-control" name="description" id="description">
+                        {{$event->description}}
+                    </textarea>
+                </div>
+            </section>
+            <h3 class="flex space-x-2 justify-center font-bold pb-2">
+                Fecha y hora
+            </h3>
+            <section class="flex space-x-2 justify-center">
+                <div>
+                    <label for="date"></label>
+                    <input type="date" id="date" name="date" value="{{$event->date}}" min="2022-01-01" max="2022-12-31">
+                </div>
+                <div>
+                    <input value="{{$event->time}}" type="time" name="time" id="time">
+                </div>
+            </section>
+            <section id="container" class="flex space-x-2 justify-center py-6">
+                <div class="contenido pt-2 font-bold">
+                    <label><input value="{{$event->fetured}}" type="checkbox" id="featured">Destacado</label>
+                </div>
+                <div class="contenido marco2">
+                    <input value="{{$event->capacity}}" type="number" placeholder="Maximo participantes" max="99" />
+                </div>
+            </section>
+            <div class="flex space-x-2 justify-center space-evenly">
+                <button type="submit" class="inline-block px-6 py-2.5 bg-green-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out" onclick="return confirm ('!!ATENCIÓN¡¡ Debe cambiar o volver a subir la misma imagen por razones de seguridad')">
+                    Confirmar
+                </button>
+                <a class="btn inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out" href="{{route('home')}}">
+                    Cancelar
+                </a>
             </div>
-            <div class="contenido marco2">
-                <input value="{{$event->capacity}}" type="number" placeholder="Maximo participantes" max="99" />
-            </div>
-        </section>
-        <div class="flex space-x-2 justify-center space-evenly">
-            <button type="submit" class="inline-block px-6 py-2.5 bg-green-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out" onclick="return confirm ('!!ATENCIÓN¡¡ Debe cambiar o volver a subir la misma imagen por razones de seguridad')">Confirmar</button>
-            <a class="btn inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out" href="{{route('home')}}">Cancelar</a>
-        </div>
     </form>
 @endsection
