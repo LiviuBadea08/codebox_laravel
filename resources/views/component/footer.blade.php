db7475eed16cd473e5a26757ba2a461986e94c91
LvL090 <calvod879@gmail.com>
2022-04-20 11:18:34 +0200

    pft

--- resources/views/component/footer.blade.php
+++ resources/views/component/footer.blade.php
@@ -1,11 +1,14 @@
 <nav class="new-nav text-white" id="nav">
-    <section>
-        <img src="./facebook.svg" alt="">
+    <section class="flex justify-items-center justify-evenly">
+        <img src="/resources/img/facebook.svg" alt="facebook">
+        <img src="/resources/img/instagram.jpg" alt="instagram">
+        <img src="/resources/img/twitter.svg" alt="twitter">
     </section>
+    <!--
     <section class="container__nav nav-space-between ">
         <div class="nav__logo nav-space-between text-withe ">
             <a href="/"></a>
             <a href="#" id="icon"><i class="text-withe fa-solid fa-arrow-up"></i></a>
         </div>
-    </section>
+    </section> !-->
 </nav>
