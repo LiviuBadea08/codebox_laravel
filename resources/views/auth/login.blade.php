72c2fd83292ca1b781437bb942747624391bb885
quuuercus <indexado05@gmail.com>
2022-04-25 12:30:43 +0200

    login and register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container mt-5 mb-5">
     <div></div>
     <div class="row justify-content-center">
         <div class="col-md-8">
-                <h1 class="row justify-content-center font-sans font-bold text-4xl 36px" >Iniciar Sesion</h1>
-                <div class="text-white font-sans font-bold bg-white">{{ __('Login') }}</div>
+                <h1 class="mb-3 row justify-content-center font-sans font-bold text-4xl 36px" >Iniciar Sesion</h1>
+                
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end text-base font-sans font-bold">{{ __('Email Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-end text-base font-sans font-bold">{{ __('Direccion email') }}</label>
 
                             <div class="col-md-6">
                                 <input placeholder="Escribe tu usuario" class="w-full bg-neutral-300 py-3 px-12 border hover: border-gray-500 rounded shadow text-base font-sans" class=" id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -29,7 +29,7 @@
                                 @enderror
 
                         <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end font-sans font-bold">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-end font-sans font-bold">{{ __('Contraseña') }}</label>
 
                             <div class="col-md-6">
                                 <input placeholder="Escribe tu Contraseña" class=" w-full bg-neutral-300 py-3 px-12 border hover: border-gray-500 rounded shadow text-base font-sans" id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -49,7 +49,7 @@
                                     <input class="hover:bg-emerald-400 form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                        {{ __('Recuerdame') }}
                                     </label>
                                 </div>
                             </div>
@@ -58,13 +58,15 @@
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button class="text-center w-full py-4 bg-green-400 hover:bg-green-400 rounded-md text-white" type="submit" class="bg-emerald-400 hover:bg-emerald-400 btn">
-                                    {{ __('Login') }}
+                                    {{ __('Iniciar Sesion') }}
                                 </button>
-
+                            <div class="flex flex-col ">
                                 @if (Route::has('password.request'))
                                     <a class="text-sm font-sans font-medium text-gray-600 underline"" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        {{ __('Has olvidado la contraseña?') }}
                                     </a>
+                                    <a class="text-sm font-sans font-medium text-gray-600 underline"" href="{{ route('register') }}">
+                                        {{ __('No tienes cuenta registrate!') }}
                                 @endif
                             </div>
                         </div>
