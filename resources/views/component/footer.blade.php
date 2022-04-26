85948b8fbc03f379abfe724b5292974e249945bd
jhonv4sq <jhonv4sq.01@gmail.com>
2022-04-26 15:55:11 +0200

    update

--- resources/views/component/footer.blade.php
+++ resources/views/component/footer.blade.php
@@ -1,23 +1,22 @@
-<footer class="text-center text-white" style="background-color: #34D399
-;">
+<footer class="text-center text-white bg-gray-800">
   <div class="container pt-6">
     <div class="flex justify-center mb-6">
-      <a href="https://www.facebook.com" class="mr-9 text-gray-800" target="_blank">
-      <img src="https://img.icons8.com/wired/40/000000/facebook--v2.png">
+      <a href="https://www.facebook.com" class="mr-9 text-gray-800" target="_blank" style="color:white;">
+        <i class="fa-brands fa-facebook-square" style="font-size:38px"></i>
       </a>
 
-      <a href="https://www.instagram.com" class="mr-9 text-gray-800" target="_blank">
-      <img src="https://img.icons8.com/wired/40/000000/instagram-new--v1.png">
+      <a href="https://www.instagram.com" class="mr-9 text-gray-800" target="_blank" style="color:white;">
+        <i class="fa-brands fa-instagram" style="font-size:38px"></i>
       </a>
 
-      <a href="https://www.twitter.com" class="mr-9 text-gray-800" target="_blank">
-      <img src="https://img.icons8.com/wired/40/000000/twitter--v1.png">
+      <a href="https://www.twitter.com" class="mr-9 text-gray-800" target="_blank" style="color:white;">
+        <i class="fa-brands fa-twitter-square" style="font-size:38px"></i>
       </a>
     </div>
   </div>
 
-  <div class="text-center text-gray-700 p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+  <div class="text-center text-gray-700 p-3 text-slate-400 bg-gray-900">
     © 2022 Copyright:
-    <a class="text-gray-800" href="#">Toalhitas do bebe</a>
+    <a class="text-gray-800 text-slate-500" href="#">Toalhitas do bebe</a>
   </div>
 </footer>
--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -4,7 +4,7 @@
 <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
 
 <section class="relative block h-500-px">
-    <div class="absolute top-0 w-full h-full bg-center bg-cover md:h-full" style="background-image: url('https://images.unsplash.com/photo-1499336315816-097655dcfbda?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2710&amp;q=80');">
+    <div class="absolute top-0 w-full h-full bg-center bg-cover md:h-full" style="background-image: url('https://i.pinimg.com/564x/d1/4a/60/d14a6093b8f1ff9c5b4e945695045254.jpg');">
         <span id="blackOverlay" class="w-full h-full absolute opacity-50 bg-black"></span>
     </div>
     <div class="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-100-px" style="transform: translateZ(0px)">
@@ -13,58 +13,6 @@
         </svg>
     </div>
 </section>
-
-{{-- <form action="{{route('store')}}" method="post">
-    @csrf
-    <div class="flex space-x-2 justify-center mr-12 pl-16 font-bold">
-        <h1>Añadir un evento</h1>
-    </div>
-    <form class=" col-sm-4 p-4">
-        <h3 class="flex space-x-2 justify-center font-bold pb-2">Imagen</h3>
-        <section class="flex space-x-2 justify-center pb-2">
-            <div class="marco2">
-                <input type="text" name="image" id="image">
-            </div>
-        </section>
-        <h3 class="flex space-x-2 justify-center font-bold pb-2">Titulo</h3>
-        <section class="flex space-x-2 justify-center pb-2">
-            <div class="marco2">
-                <input type="text" name="name" aria-describedby="titulo" id="name" required>
-            </div>
-        </section>
-        <h3 class="flex space-x-2 justify-center font-bold pb-2">Descripción</h3>
-        <section class="flex space-x-2 justify-center pb-6">
-            <div class="marco2">
-                <textarea rows="4" cols="20" name="description" id="description" required></textarea>
-            </div>
-        </section>
-        <h3 class="flex space-x-2 justify-center font-bold pb-2">Fecha y hora</h3>
-        <section class="flex space-x-2 justify-center">
-            <div>
-                <label for="start"></label>
-                <input type="date" name="date" id="date" min="0000-00-00" max="2022-12-31">
-            </div>
-            <div>
-                <input type="time" name="time" id="time">
-            </div>
-        </section>
-        <section id="container" class="flex space-x-2 justify-center py-6">
-            <div class="contenido pt-2 font-bold">
-                <input type="checkbox" name="featured" id="featured">
-                <label for="featured">Destacado</label>
-            </div>
-            <div class="contenido marco2">
-                <input type="number" name="capacity" id="capacity" placeholder="Maximo participantes" min="1" max="10" />
-            </div>
-        </section>
-        <div class="mb-5 flex space-x-2 justify-center space-evenly">
-            <button type="submit" class="inline-block px-6 py-2.5 bg-green-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out" onclick="return confirm ('Seguro que desea crear este evento?')">Confirmar</button>
-            <!--  create button that redirects to home -->
-            <a href="{{route('home')}}" class="btn inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out">Cancelar</a>
-        </div>
-    </form>
-     --}}
-
 
      <section class="relative py-16 bg-blueGray-200">
             <div class="container mx-auto px-4">
