fc7ed94aeeb075245309760dfc9162b45f3ed128
Oriol Codina Vallori <oriol.codina10@gmail.com>
2022-04-29 10:44:32 +0200

    fix factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        $event = Event::factory()->create([
+        Event::factory()->create([
             'name' => 'Master Class Tailwind CSS',
             'description' => 'Actualmente, está habiendo una creciente ola de desarrollo web rápido y diseños responsive con constantes avances tecnológico. El comienzo de Tailwind CSS es un importante contribuyente a esta evolución y se está ya utilizando en todo el mundo.
 
@@ -35,7 +35,7 @@ class DatabaseSeeder extends Seeder
             'stock' => '2',
             'featured' => true,]);
 
-        $event = Event::factory()->create([
+        Event::factory()->create([
             'name' => 'Webinar Redes',
             'description' => 'A fin de que puedas ampliar tus conocimientos en los sistemas informáticos y gracias a un completo temario formativo, podrás aprender a implantar sistemas operativos y a planificar y administrar redes, analizarás los fundamentos de hardware para comprender las bases de datos, su diseño y gestión, podrás conocer y dominar los lenguajes de marcas y sistemas de gestión de información, que permitan administrar los sistemas operativos, podrás comprender el concepto de servicios de internet e implantación de aplicaciones web, además de administrar los sistemas gestores de bases de datos.',
             'price' => '10',
@@ -45,7 +45,7 @@ class DatabaseSeeder extends Seeder
             'stock' => '3',
             'featured' => true,]);
 
-        $event = Event::factory()->create([
+        Event::factory()->create([
             'name' => ' Webinar Mi primer montaje de un PC!',
             'description' => 'Cada vez vemos más empresas que se centran en crear dispositivos diseñados para despertar curiosidad e interés en la gente, especialmente en los más pequeños. Saber, o al menos tener una ligera idea de cómo es un ordenador por dentro, puede despertar algo en el interior de aquellas personas que todavía no tienen muy claro qué quieren ser.
 
@@ -58,7 +58,7 @@ class DatabaseSeeder extends Seeder
             'featured' => true,]);
 
         
-        $event = Event::factory()->create([
+        Event::factory()->create([
             'name' => 'Master Class SCRUM metodologías ágiles',
             'description' => 'En esta completa formación aprendes sobre Lean Startup, vas a ver ejemplos prácticos creados aplicando agile. Te formas con fundamentos y herramientas de prototipado, también vas a ver estrategias y modelos de negocio, diferentes metodologías como Kanban y Scrum. Encuentras en esta formación la eficacia que buscas para salir al mercado laboral.',
             'price' => '10',
@@ -68,7 +68,7 @@ class DatabaseSeeder extends Seeder
             'stock' => '20',
             'featured' => true,]);
         
-        $event = Event::factory()->create([
+        Event::factory()->create([
             'name' => 'Master Class CleanCode',
             'description' => 'Clean Code, o Código Limpio, es una filosofía de desarrollo de software que consiste en aplicar técnicas simples que facilitan la escritura y lectura de un código, volviéndolo más fácil de entender.
 
@@ -82,7 +82,7 @@ class DatabaseSeeder extends Seeder
             'stock' => '15',
             'featured' => false,]);
 
-        $event = Event::factory()->create([
+        Event::factory()->create([
             'name' => 'Master Class ciberseguridad,protégete! ',
             'description' => 'Adquiere conocimientos para configurar, administrar y mantener la seguridad de los sistemas informáticos, garantizando la funcionalidad, la integridad de los recursos y servicios del sistema. Los profesionales con esta titulación pueden ejercer sus actividades en el área de informática de cualquier organización que dispongan de sistemas para la gestión de datos e infraestructura de redes (intranet, internet y/o extranet).',
             'price' => '10',
@@ -92,7 +92,7 @@ class DatabaseSeeder extends Seeder
             'stock' => '3',
             'featured' => true,]);
         // eventos pasados
-        $event = Event::factory()->create([
+        Event::factory()->create([
             'name' => 'Mi primer: Hola Mundo!',
             'description' => 'Master Class introductorio para niñas y niños en el mundo de la programacion web',
             'price' => '10',
@@ -102,7 +102,7 @@ class DatabaseSeeder extends Seeder
             'stock' => '12',
             'featured' => false,]);
 
-        $event = Event::factory()->create([
+        Event::factory()->create([
             'name' => 'Principios Para Desarrollar Software de Calidad',
             'description' => 'Una de las claves profesionales del Ingeniero Informático es dominar el arte y la técnica de la programación, de la codificación, una actividad necesaria que sigue al análisis y diseño de una solución software. Conocer los principios SOLID nos puede ayudar a generar soluciones de más calidad, es decir, correctas, robustas y modulares.',
             'price' => '10',
@@ -112,7 +112,7 @@ class DatabaseSeeder extends Seeder
             'stock' => '12',
             'featured' => false,]);
 
-        $event = Event::factory()->create([
+        Event::factory()->create([
             'name' => 'Master Class Robotica Avanzada',
             'description' => 'Este proyecto conjunto de la la Escuela Politécnica de Cuenca, la Escuela Técnica Superior de Ingeniería Industrial de Ciudad Real, la Escuela de Ingeniería Industrial y Aeroespacial de Toledo y la Escuela Técnica Superior de Ingeniería Industrial de Albacete, tiene como objetivo principal promover la carrera universitaria y, en concreto, los estudios de Grado en Ingeniería de la UCLM a estudiantes de Enseñanza Secundaria Obligatoria (ESO) y Bachillerato. ',
             'price' => '20',
@@ -122,7 +122,7 @@ class DatabaseSeeder extends Seeder
             'stock' => '15',
             'featured' => false,]);
 
-        $event = Event::factory()->create([
+        Event::factory()->create([
             'name' => 'Master Class introductorio de Frontend',
             'description' => 'Este curso quiere contribuir a la formación digital de nuevos profesionales con el objetivo de dar respuesta a aquellas profesiones digitales más demandadas en ese momento. El curso permitirá a los participantes adquirir los conocimientos y competencias clave para trabajar en el sector digital y mejorar sus oportunidades laborales.',
             'price' => '10',
@@ -132,7 +132,7 @@ class DatabaseSeeder extends Seeder
             'stock' => '20',
             'featured' => false,]);
 
-        $event = Event::factory()->create([
+        Event::factory()->create([
             'name' => 'Estrategias y herramientas Para el acceso al mercado de trabajo',
             'description' => 'Orientación laboral para el alumno de Ingeniería Informática - Estrategias para acceder al mercado de trabajo - Currículum Vítae - Tipos de Pruebas de Selección,',
             'price' => '10',
@@ -142,7 +142,7 @@ class DatabaseSeeder extends Seeder
             'stock' => '15',
             'featured' => false,]);
 
-        $event = Event::factory()->create([
+        Event::factory()->create([
             'name' => 'Curso de Computación Cuántica. Edición 3.0.',
             'description' => 'La computación cuántica es un paradigma de programación basado en los principios de la mecánica cuántica. El uso de los computadores cuánticos incrementará la capacidad de cálculo de hoy en día. Es por ello que está despertando gran interés en diferentes sectores científicos industriales. El presente curso pretende establecer unas bases de conocimiento sobre la computación cuántica, que supondrá un primer paso en la formación de los estudiantes y/o profesionales que quieran acercarse y sientan curiosidad por esta emergente área científica. El programa del curso se estructura de manera comprensible e incremental orientándose hacia la práctica sin descuidar los principios.',
             'price' => '10',
@@ -155,7 +155,7 @@ class DatabaseSeeder extends Seeder
 
             
 
-        $user = User::factory()->create([
+        User::factory()->create([
             'name' => 'user1',
             'email' => 'user1@gmail.com',
             
@@ -177,8 +177,6 @@ class DatabaseSeeder extends Seeder
             'isAdmin' => true,
         ]);
 
-
-        $user -> event () -> attach ( $event );
     }
     
 
